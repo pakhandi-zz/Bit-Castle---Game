e5ccb9dc37687ef69fc42dc18e7fece7bccd4620
pakhandi <asimkprasad@gmail.com>
2015-01-31 01:33:11 +0530

    Primary_build

--- src/bit_castle.py
+++ src/bit_castle.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from Tkinter import *
 
 pygame.init()
 pygame.display.init()
@@ -11,6 +12,7 @@ wall_color = (240,150,0)
 red = (255,0,0)
 green = (0, 155, 0)
 gate_color = (255,0,0)
+grey = (128,128,128)
 (width, height) = (200, 700)
 
 wall_x = []
@@ -24,13 +26,12 @@ death_y = []
 
 recharge_x = []
 recharge_y = []
-initial_charge_array = [  700  ,  700  ,  700  ,  700  ,  400  ,  400  ,  400  ,  400  ,  300  ,  300  ]
-clock_tick_array =     [  80   ,  70   ,  70   ,  50   ,  40   ,  40   ,  30   ,  30   ,  30   ,  30   ]
-new_recharge_array =   [  140  ,  130  ,  100  ,  100  ,  120  ,  120  ,  130  ,  130  ,  150  ,  150  ]
-max_deaths_array =     [  2    ,  3    ,  4    ,  4    ,  6    ,  6    ,  6    ,  6    ,  6    ,  6    ]
-max_recharges_array =  [  6    ,  6    ,  6    ,  6    ,  6    ,  6    ,  5    ,  5    ,  4    ,  4    ]
-
-
+initial_charge_array = [  700  ,  700  ,  700  ,  700  ,  400  ,  400  ,  400  ,  400  ,  300  ,  300  ,  100 ]
+clock_tick_array =     [  80   ,  70   ,  70   ,  50   ,  50   ,  50   ,  50   ,  50   ,  50   ,  50   ,  50  ]
+new_recharge_array =   [  140  ,  130  ,  100  ,  100  ,  120  ,  120  ,  130  ,  130  ,  150  ,  150  ,  100 ]
+max_deaths_array =     [  2    ,  3    ,  4    ,  4    ,  6    ,  6    ,  6    ,  6    ,  6    ,  6    ,  10  ]
+max_recharges_array =  [  6    ,  6    ,  6    ,  6    ,  6    ,  6    ,  5    ,  5    ,  4    ,  4    ,  4   ]
+dead_time_array =      [  1800 ,  1500 ,  1500 ,  1200 ,  1200 ,  1100 ,  1100 ,  1000 ,  1000 ,  800  ,  800 ]
 
 global DEATHS
 global RECHARGES
@@ -45,17 +46,74 @@ global isrunning
 global curr_score
 curr_score = 0
 
+global DELAY
+DELAY = 300
+
+global EXTRA_CHARGE
+EXTRA_CHARGE = 0
+
 global INITIAL_CHARGE, NEW_RECHARGE, CLOCK_TICK
 
-def start_Screen():
-	screen = pygame.display.set_mode((width, height))
-	font1 = pygame.font.SysFont('Arial', 20)
+def init_game():
+	global DELAY, EXTRA_CHARGE, curr_score
+	DELAY =300
+	EXTRA_CHARGE = 0
+	curr_score = 0
+	return
+
+def rules():
+	screen1 = pygame.display.set_mode((width+800, height))
+	font1 = pygame.font.SysFont('comicsansms', 18)
 	font2 = pygame.font.SysFont('comicsansms', 30)
+	font3 = pygame.font.SysFont('comicsansms', 20)
 	pygame.display.set_caption("Bit Castle")
-	screen.fill(background_color)
-	screen.blit(font2.render("Bit Castle", True, green), (35,100))
-	screen.blit(font1.render("Press P to Play",True, (0,0,0)), (45,200))
-	pygame.display.flip()
+	screen1.fill(background_color)
+
+	batterypic = pygame.image.load('battery.png')
+	killpic = pygame.image.load('kill.png')
+	gatepic = pygame.image.load('door.png')
+	bigbatterypic = pygame.image.load('bigbattery.png')
+	keypic = pygame.image.load('key.png')
+	coinpic = pygame.image.load('coin.png')
+
+	isrunning = True
+	while isrunning:
+		event = pygame.event.poll()
+		if event.type == pygame.QUIT:
+			sys.exit(0)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_r:
+				isrunning = False
+				return
+		screen1.fill(background_color)
+		screen1.blit(font2.render("Rules & Controls", True, green), (380,50))
+		screen1.blit(font1.render("Press R to Return",True, (0,0,0)), (420,100))
+		screen1.blit(font1.render("1>  Goal of the game is to reach the door in each level.", True, (0,0,0)), ( 10 , 140 ))
+		screen1.blit(font1.render("2>  Each level is completed only when the player reaches the gate with the Key.", True, (0,0,0)), ( 10 , 170 ))
+		screen1.blit(font1.render("3>  Without taking the key, the level won't be over.", True, (0,0,0)), ( 10 , 200 ))
+		screen1.blit(font1.render("4>  You have got a torch, it'll make it possible for you to see the objects in a level.", True, (0,0,0)), ( 10 , 230 ))
+		screen1.blit(font1.render("5>  The torch can be turned OFF or ON using Space-Bar.", True, (0,0,0)), ( 10 , 260 ))
+		screen1.blit(font1.render("6>  When the torch is OFF, you can only see yourself and nothing else.", True, (0,0,0)), ( 10 , 290 ))
+		screen1.blit(font1.render("7>  If the battery of the torch runs out, the torch is put-off automatically.", True, (0,0,0)), ( 10 , 320 ))
+		screen1.blit(font1.render("8>  If you decide to put OFF the torch, the torch can be put back ON after a certain amout of time ONLY.", True, (0,0,0)), ( 10 , 350 ))
+		screen1.blit(font1.render("9>  There are some 'red' colored crosses, touching them will kill you instantaneously.", True, (0,0,0)), ( 10 , 380 ))
+		screen1.blit(font1.render("10>  There are some 'green' colored batteries, touching them will recharge your torch batteries a little.", True, (0,0,0)), ( 10 , 410 ))
+		screen1.blit(font1.render("11>  You have to reach the gate in a certain amount of time, else the Game Is Over.", True, (0,0,0)), ( 10 , 440 ))
+		screen1.blit(font1.render("12>  After every level, you can visit the store and buy some boosts to help you in coming levels.", True, (0,0,0)), ( 10 , 470 ))
+		screen1.blit(font1.render("13>  The left-over battery charge will be converted to coins at the end of any level.", True, (0,0,0)), ( 10 , 500 ))
+		screen1.blit(font1.render("14>  Press Z to PAUSE.", True, (0,0,0)), ( 10 , 530 ))
+		screen1.blit(gatepic, (10, 570))
+		screen1.blit(font1.render("The door", True, (0,0,0)), (30, 560))
+		screen1.blit(killpic, (10, 590))
+		screen1.blit(font1.render("Death", True, (0,0,0)), (30, 580))
+		screen1.blit(batterypic, (10, 610))
+		screen1.blit(font1.render("Recharge", True, (0,0,0)), (30, 600))
+		pygame.display.update()
+				
+		
+def start_Screen():
+	init_game()
+	
 	isrunning = True
 	while isrunning:
 		for event in pygame.event.get():
@@ -63,13 +121,24 @@ def start_Screen():
 				sys.exit(0)
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_p:
+					screen.fill(background_color)
 					return
-				if event.key == pygame.K_q:
-					sys.exit(0)
+				if event.key == pygame.K_r:
+					rules()
+		screen = pygame.display.set_mode((width, height))
+		font1 = pygame.font.SysFont('comicsansms', 15)
+		font2 = pygame.font.SysFont('comicsansms', 30)
+		pygame.display.set_caption("Bit Castle V-1.0.0")
+		screen.fill(background_color)
+		screen.blit(font2.render( "Bit Castle", True, green), (35,100) )
+		screen.blit(font1.render( "Press P to Play",True, (0,0,0)), (50,200))
+		screen.blit(font1.render( "Press R for Rules & Controls",True, (0,0,0)), (2,250))
+		screen.blit(font1.render( "By -- Asim Krishna Prasad",True, (0,0,0)), (10,320))
+		pygame.display.flip()
 
 def pause_Screen(curr_score):
 	screen = pygame.display.set_mode((width, height))
-	font1 = pygame.font.SysFont('Arial', 20)
+	font1 = pygame.font.SysFont('comicsansms', 15)
 	font2 = pygame.font.SysFont('comicsansms', 30)
 	pygame.display.set_caption("Bit Castle")
 	screen.fill(background_color)
@@ -91,10 +160,100 @@ def pause_Screen(curr_score):
 				if event.key == pygame.K_q:
 					sys.exit(0)
 
+global PLAYER,  entry2, input2
+
+
+def get_log_var(w1):
+	global PLAYER
+	PLAYER = entry2.get()
+	#print PLAYER
+	#destroyit(w1)
+	#w1.destroy()
+	return
+
+def get_log_var2(event):
+	global w1
+	global PLAYER
+	PLAYER = entry2.get()
+	#print PLAYER
+	#destroyit(w1)
+	w1.destroy()
+	return
+
+def getusername():
+	global w1
+	w1 = Tk()
+
+	w1.title("Bit Castle")
+	w1.geometry("300x100+350+120")
+
+	global entry2
+	global input2
+
+	label2 = Label(w1,text="Enter your Name : ", height=2)
+	label2.grid(row=4, column=4)
+
+	input2 = StringVar()
+	entry2 = Entry(w1, textvariable = input2)
+	btndec = Button(w1, text="Submit", command=lambda : get_log_var(w1))
+	
+	#btndec['get_log_var(w1)'] = w1.destroy()
+	entry2.grid(row=4, column=7 )
+	btndec.grid(row=5, column=5)
+	entry2.bind("<Return>", get_log_var2)
+	entry2.focus_set()
+	w1.mainloop()
+
+def gameover_Screen():
+	global curr_score
+	global DELAY
+	global EXTRA_CHARGE
+	global PLAYER
+
+	length = len(PLAYER)
+	length = length * 13
+	spaces = 400 - length
+	start_x = spaces/2
+
+	screen = pygame.display.set_mode((width+200, height))
+	font1 = pygame.font.SysFont('comicsansms', 15)
+	font2 = pygame.font.SysFont('comicsansms', 30)
+	font3 = pygame.font.SysFont('comicsansms', 20)
+	pygame.display.set_caption("Bit Castle")
+	screen.fill(background_color)
+	
+	pygame.display.flip()
+	isrunning = True
+	while isrunning:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit(0)
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_p or event.key == pygame.K_z:
+					return
+				if event.key == pygame.K_q:
+					sys.exit(0)
+		screen.fill(background_color)
+		screen.blit(font2.render("Bit Castle", True, green), (148,100))
+		screen.blit(font2.render("Congrats", True, (37,14,182)), (150, 150))
+		screen.blit(font2.render(PLAYER, True, green), (start_x, 200))
+		coinpic = pygame.image.load('coin.png')
+		screen.blit(coinpic, (190, 265))
+		screen.blit(font1.render(str(curr_score), True, (0,0,0)), (210,260))
+		
+		screen.blit(font1.render("Take a snapshot and share it on fb.com/bugecode", True, (0,0,0)), (25, 300))
+		screen.blit(font1.render("Press Z to Start Again", True, (0,0,0)), (140, 350))
+		pygame.display.update()
+
+
 def start_level(nlevel):
 
-	sound = pygame.mixer.Sound("c.wav")
-	doclaps = pygame.mixer.Sound("claps.wav")
+	if nlevel >= 10:
+		ind = 10
+	else:
+		ind = nlevel
+	#sound = pygame.mixer.Sound("c.wav")
+	#doclaps = pygame.mixer.Sound("claps.wav")
 
 	del wall_x[:]
 	del wall_y[:]
@@ -112,21 +271,24 @@ def start_level(nlevel):
 
 	global INITIAL_CHARGE
 	global CLOCK_TICK, NEW_RECHARGE
+	global EXTRA_CHARGE
 
-	INITIAL_CHARGE = initial_charge_array[nlevel]
+	INITIAL_CHARGE = initial_charge_array[ind]
+	INITIAL_CHARGE += EXTRA_CHARGE
 	battery = INITIAL_CHARGE
 
 	isdead = False
 
-	CLOCK_TICK = clock_tick_array[nlevel]
-	NEW_RECHARGE = new_recharge_array[nlevel]
+	CLOCK_TICK = clock_tick_array[ind]
+	NEW_RECHARGE = new_recharge_array[ind]
 
-	MAX_DEATHS = max_deaths_array[nlevel]
-	MAX_RECHARGES = max_recharges_array[nlevel]
+	MAX_DEATHS = max_deaths_array[ind]
+	MAX_RECHARGES = max_recharges_array[ind]
+	
+	global DELAY
 
+	font1 = pygame.font.SysFont('comicsansms', 15)
 
-
-	font1 = pygame.font.SysFont('Arial', 20)
 
 	global isrunning
 	isrunning = True
@@ -148,10 +310,12 @@ def start_level(nlevel):
 	got_key = False
 	key_y = random.randint(260,280)
 	key_x = random.randint(10,180)
-	print key_x
-	print key_y
+	#print key_x
+	#print key_y
 
 	global curr_score
+
+	
 
 
 	global DEATHS
@@ -160,7 +324,7 @@ def start_level(nlevel):
 	RECHARGES = 0
 
 	curr_x = 90
-	curr_y = 590
+	curr_y = 570
 	last_x = 90
 	last_y = 590
 	dir_x_change = 0
@@ -188,6 +352,14 @@ def start_level(nlevel):
 			death_x.append(curr_death_x)
 			death_y.append(curr_death_y)
 			DEATHS = DEATHS + 1
+			if nlevel > 9 and DEATHS < MAX_DEATHS:
+				curr_death_x = random.randint(10,40)
+				curr_death_y = i+curr_death_x
+				curr_death_x = random.randint(0,190)
+				death_x.append(curr_death_x)
+				death_y.append(curr_death_y)
+				DEATHS = DEATHS + 1
+
 		if i%100==0 and RECHARGES<MAX_RECHARGES:
 			curr_recharge_x = random.randint(10,40)
 			curr_recharge_y = i+curr_recharge_x
@@ -255,12 +427,12 @@ def start_level(nlevel):
 						torch = False
 					elif torch:
 						torch = False
-						target_time = curr_time + 300
+						target_time = curr_time + DELAY
 					elif curr_time > target_time:
 						torch = True
-					print curr_time
-					print target_time
-					print ""
+					#print curr_time
+					#print target_time
+					#print ""
 
 
 		if curr_x >= key_x and curr_x <= key_x+10 and curr_y >= key_y and curr_y<=key_y+10:
@@ -285,7 +457,9 @@ def start_level(nlevel):
 
 		if reached_gate and got_key:
 			curr_score+=battery
-			channel = doclaps.play()
+			#getusername()
+			#gameover_Screen()
+			#channel = doclaps.play()
 			return
 
 		for i in range(DEATHS):
@@ -325,27 +499,27 @@ def start_level(nlevel):
 				break;
 
 
-		sound.set_volume(0.2)
+		#sound.set_volume(0.2)
 		for i in range(WALLS):
 			if ( (curr_x < wall_x[i]+wall_length[i] and curr_x >= wall_x[i]) or (curr_x+10 < wall_x[i]+wall_length[i] and curr_x+10 > wall_x[i]) ) and curr_y <= wall_y[i]+10 and curr_y>=wall_y[i]:
 				touched_wall_lower = True
-				channel = sound.play()
-				sound.set_volume(0.2)
+				#channel = sound.play()
+				#sound.set_volume(0.2)
 
 			if ( (curr_x < wall_x[i]+wall_length[i] and curr_x >= wall_x[i]) or (curr_x+10 < wall_x[i]+wall_length[i] and curr_x+10 > wall_x[i]) ) and curr_y+10 <= wall_y[i]+10 and curr_y+10 >=wall_y[i]:
 				touched_wall_upper = True
-				channel = sound.play()
-				sound.set_volume(0.2)
+				#channel = sound.play()
+				#sound.set_volume(0.2)
 
 			if curr_x >= wall_x[i] and curr_x <= wall_x[i]+wall_length[i] and ( (curr_y >= wall_y[i] and curr_y < wall_y[i]+wall_width[i]) or (curr_y+10 > wall_y[i] and curr_y+10 <= wall_y[i]+wall_width[i])):
 				touched_wall_right = True
-				channel = sound.play()
-				sound.set_volume(0.2)
+				#channel = sound.play()
+				#sound.set_volume(0.2)
 
 			if curr_x+10 >= wall_x[i] and curr_x+10 <= wall_x[i]+wall_length[i] and ( (curr_y >= wall_y[i] and curr_y < wall_y[i]+wall_width[i]) or (curr_y+10 > wall_y[i] and curr_y+10 <= wall_y[i]+wall_width[i])):
 				touched_wall_left = True
-				channel = sound.play()
-				sound.set_volume(0.2)
+				#channel = sound.play()
+				#sound.set_volume(0.2)
 
 		if curr_x <= 0:
 			touched_wall_right = True
@@ -353,14 +527,20 @@ def start_level(nlevel):
 			touched_wall_left = True
 		if curr_y <= 0:
 			touched_wall_lower = True
-		if curr_y+10 > height:
+		if curr_y+10 > 580:
 			touched_wall_upper = True
 
 		if curr_x >= width or curr_x < 0 or curr_y < 0 or curr_y >= height:
 			doexit = True
 
+		if curr_time > dead_time_array[ind]:
+			isdead = True
+
 		if isdead:
-			sys.exit(0)
+			getusername()
+			gameover_Screen()
+			main()
+			#start_Screen()
 
 		if touched_wall_upper and dir_y_change>0:
 			curr_y = curr_y
@@ -427,8 +607,8 @@ def start_level(nlevel):
 		pygame.draw.rect( screen, (0,0,0), (0,580,200,120))
 		screen.blit(font1.render(str(battery),True, (255,255,255)), (30,580))
 		screen.blit(font1.render("Level : "+str(nlevel+1),True, (255,255,255)), (55,680))
-		screen.blit(coinpic, (20, 650))
-		screen.blit(font1.render(str(curr_score), True, (255,255,255)), (40, 645))
+		screen.blit(coinpic, (20, 645))
+		screen.blit(font1.render(str(curr_score), True, (255,255,255)), (40, 640))
 		if not got_key:
 			screen.blit(font1.render("Key??",True, (255,255,255)), (80,580))
 		else:
@@ -437,17 +617,92 @@ def start_level(nlevel):
 			screen.blit(font1.render("Torch will be back in : "+str(target_time - curr_time),True, (255,255,255)), (10,615))
 		else:
 			screen.blit(font1.render("Torch ready",True, (255,255,255)), (10,615))
+		screen.blit(font1.render("Time Left : "+str(dead_time_array[ind] - curr_time), True, (255,255,255)), (30, 660))
 		screen.blit(bigbatterypic, (8,580))
 		pygame.display.update()
 
 		clock.tick(CLOCK_TICK)
 	
+def store():
+	global curr_score
+	global DELAY
+	global EXTRA_CHARGE
+
+	screen = pygame.display.set_mode((width+200, height))
+	font1 = pygame.font.SysFont('comicsansms', 20)
+	font2 = pygame.font.SysFont('comicsansms', 30)
+	font3 = pygame.font.SysFont('comicsansms', 20)
+	font4 = pygame.font.SysFont('comicsansms', 15)
+	pygame.display.set_caption("Bit Castle")
+	screen.fill(background_color)
+	
+	
+	pygame.display.flip()
+	isrunning = True
+	while isrunning:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit(0)
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_p or event.key == pygame.K_z:
+					return
+				if event.key == pygame.K_q:
+					sys.exit(0)
+				if curr_score >= 1200:
+					if event.key == pygame.K_1:
+						curr_score-=1200
+						DELAY -= 20
+				if curr_score >= 800:
+					if event.key == pygame.K_2:
+						curr_score -= 800
+						EXTRA_CHARGE += 25
+				if curr_score >= 1200:
+					if event.key == pygame.K_3:
+						curr_score -= 1200
+						EXTRA_CHARGE += 50
+		screen.fill(background_color)
+		screen.blit(font4.render("You have extra charge of : "+str(EXTRA_CHARGE), True, green), (10,40))
+		screen.blit(font4.render("You have reduced delay by : "+str(300-DELAY), True, green), (10,60))
+		screen.blit(font2.render("STORE", True, green), (148,100))
+		screen.blit(font1.render("Press Z to Resume",True, (0,0,0)), (130,200))
+		coinpic = pygame.image.load('coin.png')
+		screen.blit(coinpic, (160, 245))
+		screen.blit(font1.render(str(curr_score), True, (0,0,0)), (180,240))
+
+		if curr_score < 1200:
+			screen.blit(font3.render("1> Reduce Torch Lag by 20", True, grey), (10, 280) )
+		else:
+			screen.blit(font3.render("1> Reduce Torch Lag by 20", True, green), (10, 280) )
+
+		screen.blit(coinpic, (330, 288))
+		screen.blit(font1.render("1200", True, (0,0,0)), (350, 282))
+
+		if curr_score < 800:
+			screen.blit(font3.render("2> Increase initial battery by 25", True, grey), (10, 310) )
+		else:
+			screen.blit(font3.render("2> Increase initial battery by 25", True, green), (10, 310) )
+
+		screen.blit(coinpic, (330, 318))
+		screen.blit(font1.render("800", True, (0,0,0)), (350, 312))
+
+		if curr_score < 1200:
+			screen.blit(font3.render("3> Increase initial battery by 50", True, grey), (10, 340) )
+		else:
+			screen.blit(font3.render("3> Increase initial battery by 50", True, green), (10, 340) )
+
+		screen.blit(coinpic, (330, 348))
+		screen.blit(font1.render("1200", True, (0,0,0)), (350, 342))
+
+		pygame.display.update()
 
 
 def main():
+	#getusername()
+	#gameover_Screen()
 	start_Screen()
-	for i in range(10):
+	for i in range(100):
 		start_level(i)
+		store()
 
 if __name__ == "__main__":
 	main()
